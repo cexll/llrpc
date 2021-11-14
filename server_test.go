67b3a1745826c78e691114e11dfc0607b525448c
cexll <evanxian9@gmail.com>
2021-11-14 21:08:50 +0800

    add test

--- server_test.go
+++ server_test.go
@@ -1,14 +1,12 @@
 package llrpc
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"sync"
 	"testing"
 	"time"
-
-	"github.com/cexll/llrpc/codec"
 )
 
 func startServer(addr chan string) {
@@ -23,28 +21,26 @@ func startServer(addr chan string) {
 }
 
 func TestClient(t *testing.T) {
+	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
+	client, _ := Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
-	// in fact, following code is like a simple llrpc client
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() {
-		_ = conn.Close()
-	}()
 	time.Sleep(time.Second)
-	// send options
-	_ = json.NewEncoder(conn).Encode(DefaultOption)
-	cc := codec.NewGobCodec(conn)
 	// send request & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-		_ = cc.Write(h, fmt.Sprintf("llrpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("llrpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error: ", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
 	}
+	wg.Wait()
 }
